006a6ba88f42d84b8c9cfa26f838cd0f809920f8
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-06-06 21:35:12 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
-import React, { useState, useMemo, useCallback } from 'react'
+import React, { useState, useMemo } from 'react'
 import '../styles/App.css';
 
-const isArmstrongNumber = useCallback((number) => {
+
+const isArmstrongNumber = (number) => {
   console.log("Expensive calculation execution");
   const numberOfDigits = `${number}`.length;
   let sum = 0;
@@ -12,64 +13,23 @@ const isArmstrongNumber = useCallback((number) => {
     temp = parseInt(temp / 10);
    }
   return sum === number;
-}, []);
-
+};
 const App = () => {
   const [num, setNum] = useState('');
   const [count, setCount] = useState(1);
-  const isArmstrong = useMemo(() => isArmstrongNumber(num), [isArmstrongNumber, num]);
-
-  const handleInputChange = useCallback((e) => {
-    setNum(Number(e.target.value));
-  }, []);
-
+  const isArmstrong = useMemo(()=>isArmstrongNumber(num),[num]);
+ 
   return (
     <div className="App">
-      <input id='input' value={num} onChange={handleInputChange} />
+      <input id='input' value={num} onChange={(e) => setNum(+e.target.value)}/>
       <h2 id='answer'>{num ? (isArmstrong ? `${num} is armstrong` : `${num} is not armstrong`) : `Please Enter Number `}</h2>
-      <h4 id='count' onClick={() => { setCount((count) => count + 1); }}>
+      <h4 id='count'onClick={() => { setCount((count) => count + 1);}}>
         Click me to increase count:-  {count}
       </h4>
+
     </div>
   );
 }
 
+
 export default App;
-
-// import React, { useState, useMemo, useCallback } from 'react'
-// import '../styles/App.css';
-
-// const isArmstrongNumber = useCallback((number) => {
-//   console.log("Expensive calculation execution");
-//   const numberOfDigits = `${number}`.length;
-//   let sum = 0;
-//   let temp = number;
-//   while (temp > 0) {
-//     let remainder = temp % 10;
-//     sum += remainder ** numberOfDigits;
-//     temp = parseInt(temp / 10);
-//    }
-//   return sum === number;
-// }, []);
-
-// const App = () => {
-//   const [num, setNum] = useState('');
-//   const [count, setCount] = useState(1);
-//   const isArmstrong = useMemo(() => isArmstrongNumber(num), [isArmstrongNumber, num]);
-
-//   const handleInputChange = useCallback((e) => {
-//     setNum(Number(e.target.value));
-//   }, []);
-
-//   return (
-//     <div className="App">
-//       <input id='input' value={num} onChange={handleInputChange} />
-//       <h2 id='answer'>{num ? (isArmstrong ? `${num} is armstrong` : `${num} is not armstrong`) : `Please Enter Number `}</h2>
-//       <h4 id='count' onClick={() => { setCount((count) => count + 1); }}>
-//         Click me to increase count:-  {count}
-//       </h4>
-//     </div>
-//   );
-// }
-
-// export default App;
